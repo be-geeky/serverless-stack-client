9a60bdfee124ad3a0c3717c8a61ca414c1ad2a0f
Dushyant Joshi <dushyant.joshi@alogic.co>
2020-04-19 05:16:22 +0930

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-south-1",
+      BUCKET: "notes-app-uploads-dush"
+    },
+    apiGateway: {
+      REGION: "ap-south-1",
+      URL: " https://ckhiq42q9c.execute-api.ap-south-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-south-1",
+      USER_POOL_ID: "ap-south-1_Ermkgb8FR",
+      APP_CLIENT_ID: "43q130gjdrtruhrv48gduqcu5e",
+      IDENTITY_POOL_ID: "ap-south-1:d31fca63-123b-4b07-8104-24178439f2d0"
+    }
+  };
